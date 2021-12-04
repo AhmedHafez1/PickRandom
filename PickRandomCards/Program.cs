0750755e8ccb43356c550ac42c32e19c4cfacfbb
Ahmed Hafez <Ahmed.abdelghany.Hafez@gmail.com>
2021-12-04 15:57:02 +0200

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+
+                foreach(string card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            } 
+            else
+            {
+                Console.WriteLine("This is not a valid number !");
+            }
+        }
+    }
+}
